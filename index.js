129abde060cf59e4d5fad12e97f05a9ec16d8b34
unknown <indi.bhopal@gmail.com>
2025-06-15 20:21:50 +0530

    static file with path

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const app = express();
-// Set view engine to Pug
+const path=require('path')
 app.set('view engine', 'pug');
-
 // Middleware setup
 // app.use((req,res,next)=>{ // user created middle ware
 //    console.log(req.originalUrl);
@@ -10,19 +9,18 @@ app.set('view engine', 'pug');
 // })
 app.use(express.json()); // Handles JSON requests from API testing tools like POSTMAN
 app.use(express.urlencoded({ extended: true })); // Handles URL-encoded requests from user FORM 
-app.use(express.static('public'));//Static file serving (if needed) 
+app.use(express.static(path.join(__dirname,'public')));//Static file serving 
 const router = require('./routes/route'); 
 app.use('/api', router);
-
-// default Home route
+// default Home route for pug template
+// app.get('/', (req, res) => {
+//    res.render('index', { message: 'Hello World' });
+// });
+// static file route should be used after default route only
 app.get('/', (req, res) => {
-   res.render('index', { message: 'Hello World' });
+   res.sendFile(path.join(__dirname,'public/home.html'))
 });
-//should be used after default route only
-app.get('/', (req, res) => {
-   res.sendFile('home.html')
-});
-let port=2500
+const port=2500
 app.listen(port, () => {
    console.log(`Server running on port  ${port}`);
 });
